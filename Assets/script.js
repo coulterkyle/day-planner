cce4faa13989a28baf95a6d2ac3bac627923a1ce
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2023-12-21 21:42:37 -0500

    added color function

--- Assets/script.js
+++ Assets/script.js
@@ -2,25 +2,17 @@ const currentDay = dayjs().format('dddd, MMMM D YYYY, h:mm a');
 $('#currentDay').text(currentDay);
 
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-    
+    $('.time-block').each(function(){
+        const timeSlot = $(this).attr('id');
+        const currentTime = dayjs().format('HH');
+        if (timeSlot < currentTime){
+            $(this).addClass("past");
+        } else if (timeSlot === currentTime){
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+    });
 
     function toDos() {
         $('.saveBtn').on('click', function() {
@@ -28,11 +20,12 @@ $(function () {
           const value = $(this).siblings('.description').val();
           localStorage.setItem(key, value);
         });
-    $('.time-block').each(function() {
+        $('.time-block').each(function() {
         const key = $(this).attr('id');
         const value = localStorage.getItem(key);
         $(this).children('.description').val(value);
-      });
-}
-toDos()
+        })
+        };
+    
+    toDos();
 });
