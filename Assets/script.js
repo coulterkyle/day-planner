2e48d800f32d98b06bb19247b8b5d1061308450d
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2023-12-21 22:12:27 -0500

    fixed testing errors, cleaned up code and added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
-const currentDay = dayjs().format('dddd, MMMM D YYYY, h:mm a');
-$('#currentDay').text(currentDay);
-
+    //wrapped all code that interacts with the DOM in a call to jQuery
 $(function () {
-    $('.time-block').each(function(){
-        const timeSlot = $(this).attr('id');
-        const currentTime = dayjs().format('HH');
+
+    //this is the code that allows for the time slots to change colors depending on the current time
+    $(".time-block").each(function(){
+        const timeSlot = $(this).attr("id");
+        const currentTime = dayjs().format("HH");
         if (timeSlot < currentTime){
             $(this).addClass("past");
         } else if (timeSlot === currentTime){
@@ -14,18 +14,23 @@ $(function () {
         }
     });
 
-    function toDos() {
-        $('.saveBtn').on('click', function() {
-          const key = $(this).parent().attr('id');
-          const value = $(this).siblings('.description').val();
-          localStorage.setItem(key, value);
+    //this is the code that displays the current date and time in the header section
+    const currentDay = dayjs().format("dddd, MMMM D YYYY, h:mm a");
+    $("#currentDay").text(currentDay);
+
+
+    //this is the code that saves info to local storage and allows it to be called 
+    $(function toDos() {
+        $(".saveBtn").on("click", function() {
+        const key = $(this).parent().attr("id");
+        const value = $(this).siblings(".description").val();
+        localStorage.setItem(key, value);
         });
-        $('.time-block').each(function() {
-        const key = $(this).attr('id');
+        $(".time-block").each(function() {
+        const key = $(this).attr("id");
         const value = localStorage.getItem(key);
-        $(this).children('.description').val(value);
+        $(this).children(".description").val(value);
         })
-        };
+    });
     
-    toDos();
 });
